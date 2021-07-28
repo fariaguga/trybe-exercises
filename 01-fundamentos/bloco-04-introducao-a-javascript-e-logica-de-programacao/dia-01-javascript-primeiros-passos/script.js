0e56c0f83e02fd99733f172c19b7484bc4ce275b
Gustavo <fariaguga@hotmail.com>
2021-07-28 19:13:57 -0300

    Adiciona ex 9

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script.js
@@ -156,6 +156,25 @@ else{
 }
 
 
+/* 9 - Escreva um programa que defina três números em variáveis e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele retorna false .
+Bonus: use somente um if .*/
+
+
+let number1 = 8;
+let number2 = 9;
+let number3= 12;
+
+if(number1 % 2 != 0 | number2 % 2 != 0 | number3 %2 != 0 ){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
+
+
+
+
+
 
 
 
