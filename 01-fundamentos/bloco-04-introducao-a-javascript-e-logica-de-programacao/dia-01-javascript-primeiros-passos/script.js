c1ccc2a559df9fa9916446d4be7fb25d9aeb98dc
Gustavo <fariaguga@hotmail.com>
2021-07-28 18:28:35 -0300

    Adiciona ex 5

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script.js
@@ -66,3 +66,23 @@ else{
     console.log("Negative");
 }
 
+/* 5 - Faça um programa que defina três variáveis com os valores dos três ângulos internos de um triângulo.
+ Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.*/
+
+ let ang1 = 90;
+ let ang2 = 45;
+ let ang3 = 45;
+ let soma = (ang1 + ang2 + ang3);
+
+ if(soma === 180){
+     console.log("true");
+ }
+ else{
+     console.log("false");
+ }
+
+
+
+
+
+
