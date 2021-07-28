2af6d6746e8a33479250f227905770a649666cc3
Gustavo <fariaguga@hotmail.com>
2021-07-28 18:57:14 -0300

    Adiciona ex 7

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script.js
@@ -37,6 +37,7 @@ else{
     console.log(num2);
 }
 
+
 /* 3 - Faça um programa que retorne o maior de três números.
  Defina no começo do programa três variáveis com os valores que serão comparados.*/
 
@@ -56,6 +57,7 @@ else{
 
  /* 4 - Faça um programa que, dado um valor definido numa variável, retorne "positive" se esse valor for positivo,
   "negative" se for negativo e "zero" caso contrário.*/
+
 
 
 let valor = -60;
@@ -81,9 +83,17 @@ else{
      console.log("false");
  }
 
+
+
  /* 6 - Escreva um 
  programa que receba o nome de uma peça 
- de xadrez e retorne os movimentos que ela faz. */
+ de xadrez e retorne os movimentos que ela faz.
+ Como desafio, faça o programa funcionar tanto se receber o nome de uma peça com letras maiúsculas quanto com letras minúsculas, sem aumentar a quantidade de condicionais.
+Como dica, você pode pesquisar uma função que faz uma string ficar com todas as letras minúsculas (lower case) .
+Se a peça passada for inválida, o programa deve retornar uma mensagem de erro.
+Exemplo: bishop (bispo) -> diagonals (diagonais) */
+
+
 
  let peça = "BiShop".toLowerCase();
  if(peça === "bishop"){
@@ -92,6 +102,43 @@ else{
  else{
      console.log("error");
  }
+
+
+
+ /* 7 - Escreva um programa que converte uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F. Siga essas regras:
+Porcentagem >= 90 -> A
+Porcentagem >= 80 -> B
+Porcentagem >= 70 -> C
+Porcentagem >= 60 -> D
+Porcentagem >= 50 -> E
+Porcentagem < 50 -> F
+O programa deve retornar uma mensagem de erro e encerrar se a nota passada for menor que 0 ou maior que 100.*/
+
+
+let nota = 60;
+
+if(nota >= 90 && nota <=100){
+    console.log("A");
+}
+else if(nota >= 80 && nota <=100){
+    console.log("B");
+}
+else if(nota >= 70 && nota <=100){
+    console.log("C");
+}
+else if(nota >= 60 && nota <=100){
+    console.log("D");
+}
+else if(nota >= 50 && nota <=100){
+    console.log("E");
+}
+else if(nota < 50 && nota >=0){
+    console.log("F");
+}
+else{
+    console.log("error");
+}
+
 
 
 
