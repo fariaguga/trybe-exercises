e8dd47cfbc388d9693e75379a235a5c34f885cfe
Gustavo <fariaguga@hotmail.com>
2021-07-28 19:09:23 -0300

    Adiciona ex 8

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script.js
@@ -140,6 +140,25 @@ else{
 }
 
 
+/* 8 - Escreva um programa que defina três números em variáveis e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false .
+Bonus: use somente um if .*/
+
+
+let numero1 = 5;
+let numero2 = 61;
+let numero3= 12;
+
+if(numero1 % 2 === 0 | numero2 % 2 === 0 | numero3 %2 === 0 ){
+    console.log("true:");
+}
+else{
+    console.log("false");
+}
+
+
+
+
+
 
 
 
