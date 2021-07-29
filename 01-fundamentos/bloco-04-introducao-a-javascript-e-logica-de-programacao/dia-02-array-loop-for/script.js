6569bdd05430eb8c7d4fe71c95a664a77a4caf47
Gustavo <fariaguga@hotmail.com>
2021-07-29 19:15:30 -0300

    Adiciona exercicio 7

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-loop-for/script.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-array-loop-for/script.js
@@ -74,6 +74,18 @@ for (index = 0; index < numbers.length; index +=1){
     console.log (numMax);
 }
 
+/* 6 - Descubra quantos valores ímpares existem no array e imprima o resultado. 
+Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";*/
+
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let contador = 0 ;
+for(let index = 0; index < numbers.length; index += 1){
+    if(numbers[index] % 2 === 0){
+        contador += 1;
+    }
+}
+console.log(contador);
 
 
     
