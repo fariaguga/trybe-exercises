373cd6680736d14b80059a21bcb2ee6339acdc4a
Gustavo <fariaguga@hotmail.com>
2021-07-30 16:51:17 -0300

    Adiciona ex 2

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-logica-de-programacao-e-algoritmos/script.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-logica-de-programacao-e-algoritmos/script.js
@@ -1,3 +1,9 @@
+/* 1 - Para o primeiro exercício de hoje, faça um programa 
+que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito 
+de asteriscos de lado de tamanho n . Por exemplo:*/
+
+
+
 let num = 5;
 let ast = "*";
 let line = "";
@@ -11,3 +17,17 @@ for( let index = 0 ; index < num ; index += 1){
 for(let inded = 0 ; inded < num ; inded +=1){
     console.log(line);
 } 
+
+
+/* 2 - Para o segundo exercício, faça o mesmo que antes, mas 
+que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:*/
+
+let num = 5;
+let ast = "*";
+let contador = "";
+for(let index = 0 ; index < num ; index += 1){
+    contador += ast;
+    console.log(contador);
+}
+
+
