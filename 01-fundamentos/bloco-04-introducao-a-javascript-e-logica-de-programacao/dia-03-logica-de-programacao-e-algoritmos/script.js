8999cb4a3f774a6042679a73c3cf60aec8cfef75
Gustavo <fariaguga@hotmail.com>
2021-07-30 16:39:56 -0300

    Adiciona ex 1

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-logica-de-programacao-e-algoritmos/script.js
@@ -0,0 +1,13 @@
+let num = 5;
+let ast = "*";
+let line = "";
+for( let index = 0 ; index < num ; index += 1){
+    if(num > 1){
+    line += ast;
+    }
+    
+    
+}
+for(let inded = 0 ; inded < num ; inded +=1){
+    console.log(line);
+} 
