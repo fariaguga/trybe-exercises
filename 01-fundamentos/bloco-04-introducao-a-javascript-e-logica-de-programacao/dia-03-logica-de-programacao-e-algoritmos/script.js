50f793e73ba5b6738514a77b811ab6ea04b3ff3d
Gustavo <fariaguga@hotmail.com>
2021-07-30 19:07:42 -0300

    Adiciona ex 3

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-logica-de-programacao-e-algoritmos/script.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-logica-de-programacao-e-algoritmos/script.js
@@ -18,6 +18,12 @@ for(let inded = 0 ; inded < num ; inded +=1){
     console.log(line);
 } 
 
+/*let n = 5;
+for( let index =0 ; index < n ; index += 1){
+    console.log("*".repeat(n));
+}*/
+
+
 
 /* 2 - Para o segundo exercício, faça o mesmo que antes, mas 
 que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:*/
@@ -29,5 +35,28 @@ for(let index = 0 ; index < num ; index += 1){
     contador += ast;
     console.log(contador);
 }
+
+
+
+
+/* 3 - Agora inverta o lado do triângulo. Por exemplo:
+
+     *
+    **
+   ***   
+  ****
+ *****        */
+  
+
+
+let n = 5;
+let ka = n-1;
+for( let index = 1 ; index <= n ; index += 1){
+    
+    console.log(" ".repeat(ka) + "*".repeat(index));
+    ka -= 1;
+
+}
+
 
 
