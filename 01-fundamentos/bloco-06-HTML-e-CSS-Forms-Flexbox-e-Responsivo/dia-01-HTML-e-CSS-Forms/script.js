3cac693888432e91d40b7cef0cae6e52018ab9f7
Gustavo <fariaguga@hotmail.com>
2021-08-19 09:27:03 -0300

    Modifica Requisito 1

--- 01-fundamentos/bloco-06-HTML-e-CSS-Forms-Flexbox-e-Responsivo/dia-01-HTML-e-CSS-Forms/script.js
+++ 01-fundamentos/bloco-06-HTML-e-CSS-Forms-Flexbox-e-Responsivo/dia-01-HTML-e-CSS-Forms/script.js
@@ -1,9 +1,31 @@
+function createAllStates(){
 let estadosArray =['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
 let estado = document.getElementById("estado");
 for(let index=0; index < estadosArray.length ; index += 1){
     let createEstado = document.createElement("option");
     estado.appendChild(createEstado).innerText = estadosArray[index];
     estado.appendChild(createEstado).value = estadosArray[index];
+    
+    
 
     
 }
+}
+createAllStates();
+let button = document.getElementById("clear");
+let boxs = document.querySelectorAll("input");
+
+button.addEventListener("click" ,function(){
+   
+    
+    for(let index = 0 ; index < boxs.length; index += 1){
+        boxs[index].value = "";
+        
+        
+    }
+
+})
+
+
+    
+
