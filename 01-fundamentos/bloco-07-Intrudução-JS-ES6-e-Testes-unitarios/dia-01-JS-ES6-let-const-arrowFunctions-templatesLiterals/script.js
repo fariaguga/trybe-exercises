f63c8908fe6d6e2456a9c45d41169395c7ba56cd
Gustavo <fariaguga@hotmail.com>
2021-08-28 16:23:17 -0300

    Adiciona Parte I

---
+++ 01-fundamentos/bloco-07-Intrudução-JS-ES6-e-Testes-unitarios/dia-01-JS-ES6-let-const-arrowFunctions-templatesLiterals/script.js
@@ -0,0 +1,46 @@
+function soma(num1, num2) {
+let resultado = num1+num2;
+return resultado;
+}
+console.log(soma(3,2));
+
+const soma = (num1, num2) => num1+num2;
+console.log(soma(1,7));
+
+
+
+/* PARTE I */
+ /*function testingScope(escopo) {
+    if (escopo === true) {
+      var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+      ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+      console.log(ifScope);
+    } else {
+      var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+      console.log(elseScope);
+    }
+    console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+  }
+
+  testingScope(true);*/
+
+/* Modifique a estrutura da função para que ela seja uma arrow function .
+ Modifique as concatenações para template literals .*/
+
+const testingScope = escopo => { 
+    if (escopo === true) { 
+      let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+      ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+      console.log(ifScope);
+    } else {
+      const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+      console.log(elseScope);
+    }
+  }
+
+  testingScope(true);
+  
+    
+
+
+
