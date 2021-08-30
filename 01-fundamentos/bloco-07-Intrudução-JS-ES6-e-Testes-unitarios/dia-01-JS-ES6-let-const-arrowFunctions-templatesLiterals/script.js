ac5607cd0695e9cd57faa61544a567255e479377
Gustavo <fariaguga@hotmail.com>
2021-08-30 18:08:56 -0300

    Adiciona bloco 7.1

--- 01-fundamentos/bloco-07-Intrudução-JS-ES6-e-Testes-unitarios/dia-01-JS-ES6-let-const-arrowFunctions-templatesLiterals/script.js
+++ 01-fundamentos/bloco-07-Intrudução-JS-ES6-e-Testes-unitarios/dia-01-JS-ES6-let-const-arrowFunctions-templatesLiterals/script.js
@@ -12,7 +12,7 @@ console.log(soma(1,7));
 /* PARTE I */
  /*function testingScope(escopo) {
     if (escopo === true) {
-      var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+      var ifScope = 'Não devo ser utilizadza fora do meu escopo (if)';
       ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
       console.log(ifScope);
     } else {
@@ -53,61 +53,3 @@ console.log(oddsAndEvens.sort());
 
 
 
-function votacao (votos){
-    let contadorA = 0;
-    let contadorB = 0;
-    let contadorC = 0;
-    for(let index = 0 ; index < votos.length ; index += 1){
-        if(votos[index] === "A"){
-            contadorA +=1;
-            console.log(votos[index]);             
-        }
-        else if(votos[index] === "B"){
-            contadorB +=1;
-            console.log(votos[index]);              
-        }
-        else if(votos[index] === "C"){
-            contadorC +=1;
-            console.log(votos[index]);              
-        }
-        
-    }
-    return { 
-        contadorA,
-        contadorB,
-        contadorC
-    }
-    
-}
-console.log(votacao(["A","B","B","C"]));
-
-
-function votacao (votos){
-    let contadorA = 0;
-    let contadorB = 0;
-    let contadorC = 0;
-    let resultado ="";
-    for(let index = 0 ; index < 9 ; index += 1){
-        if(votos[index] === "A"){
-            contadorA +=1;                       
-        }
-       if(votos[index] === "B"){
-            contadorB +=1;                      
-        }
-       if(votos[index] === "C"){
-            contadorC +=1;           
-        }
-        
-    }
-if (contadorA > contadorB && contadorA > contadorC){
-resultado = "A";
-}
-else if (contadorB > contadorA && contadorB > contadorC){
-resultado = "B";
-}
-else if (contadorC > contadorA && contadorC > contadorB){
-resultado = "C";
-}
-  return resultado;
-}
-console.log(votacao(["A","B","B","C","C","A","B","B","C","C"]));
