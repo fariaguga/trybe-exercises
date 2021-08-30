f163162467ecc679ce4eebdf423dcdd520714fb8
Gustavo <fariaguga@hotmail.com>
2021-08-30 18:03:52 -0300

    Adiciona mudanças

--- 01-fundamentos/bloco-07-Intrudução-JS-ES6-e-Testes-unitarios/dia-01-JS-ES6-let-const-arrowFunctions-templatesLiterals/script.js
+++ 01-fundamentos/bloco-07-Intrudução-JS-ES6-e-Testes-unitarios/dia-01-JS-ES6-let-const-arrowFunctions-templatesLiterals/script.js
@@ -24,7 +24,7 @@ console.log(soma(1,7));
 
   testingScope(true);*/
 
-/* Modifique a estrutura da função para que ela seja uma arrow function .
+/*  1 - Modifique a estrutura da função para que ela seja uma arrow function .
  Modifique as concatenações para template literals .*/
 
 const testingScope = escopo => { 
@@ -39,8 +39,75 @@ const testingScope = escopo => {
   }
 
   testingScope(true);
+
+
+  const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+  // Seu código aqui.
+console.log(oddsAndEvens.sort());
+
+  console.log(oddsAndEvens); // será necessário alterar essa linha 😉
   
     
 
 
 
+
+function votacao (votos){
+    let contadorA = 0;
+    let contadorB = 0;
+    let contadorC = 0;
+    for(let index = 0 ; index < votos.length ; index += 1){
+        if(votos[index] === "A"){
+            contadorA +=1;
+            console.log(votos[index]);             
+        }
+        else if(votos[index] === "B"){
+            contadorB +=1;
+            console.log(votos[index]);              
+        }
+        else if(votos[index] === "C"){
+            contadorC +=1;
+            console.log(votos[index]);              
+        }
+        
+    }
+    return { 
+        contadorA,
+        contadorB,
+        contadorC
+    }
+    
+}
+console.log(votacao(["A","B","B","C"]));
+
+
+function votacao (votos){
+    let contadorA = 0;
+    let contadorB = 0;
+    let contadorC = 0;
+    let resultado ="";
+    for(let index = 0 ; index < 9 ; index += 1){
+        if(votos[index] === "A"){
+            contadorA +=1;                       
+        }
+       if(votos[index] === "B"){
+            contadorB +=1;                      
+        }
+       if(votos[index] === "C"){
+            contadorC +=1;           
+        }
+        
+    }
+if (contadorA > contadorB && contadorA > contadorC){
+resultado = "A";
+}
+else if (contadorB > contadorA && contadorB > contadorC){
+resultado = "B";
+}
+else if (contadorC > contadorA && contadorC > contadorB){
+resultado = "C";
+}
+  return resultado;
+}
+console.log(votacao(["A","B","B","C","C","A","B","B","C","C"]));
