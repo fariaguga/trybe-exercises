7dae724b91ab317e3cd2b4cbb6bb8294c48fc708
Gustavo <fariaguga@hotmail.com>
2021-08-30 21:35:05 -0300

    Adiciona Requisito 3

--- 01-fundamentos/bloco-07-Intrudução-JS-ES6-e-Testes-unitarios/dia-02-Javascript-ES6-objects/script2.js
+++ 01-fundamentos/bloco-07-Intrudução-JS-ES6-e-Testes-unitarios/dia-02-Javascript-ES6-objects/script2.js
@@ -56,3 +56,31 @@ const lesson1 = {
         console.log(Object.keys(lesson2));
     }
     keysObject();
+
+
+    /* 3 - Crie uma função para mostrar o tamanho de um objeto. */
+
+    const lesson1 = {
+        materia: 'Matemática',
+        numeroEstudantes: 20,
+        professor: 'Maria Clara',
+        turno: 'manhã',
+      };
+      
+      const lesson2 = {
+        materia: 'História',
+        numeroEstudantes: 20,
+        professor: 'Carlos',
+      };
+      
+      const lesson3 = {
+        materia: 'Matemática',
+        numeroEstudantes: 10,
+        professor: 'Maria Clara',
+        turno: 'noite',
+      };
+
+    const valuesObject = () => {
+        console.log(Object.values(lesson2));
+    }
+    valuesObject();
