e86325a29358bcbe38b73bc503756bf434337bd5
Gustavo <fariaguga@hotmail.com>
2021-09-10 10:07:07 -0300

    Adiciona Exercicio 1

---
+++ 01-fundamentos/bloco-08-Introducao-a-Higher-Order-Functions/dia-04-Higher-Order-Functions-Reduce/exercise1.js
@@ -0,0 +1,16 @@
+/* 1 - Dada uma matriz, transforme em um array. */
+const assert = require('assert');
+
+const arrays = [
+  ['1', '2', '3'],
+  [true],
+  [4, 5, 6],
+];
+
+function flatten() {
+  // escreva seu código aqui
+  return arrays.reduce((acc, elemento) => acc.concat(elemento),[])
+}
+console.log(flatten());
+
+assert.deepStrictEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);
